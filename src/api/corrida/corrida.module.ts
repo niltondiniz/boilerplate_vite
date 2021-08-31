00ef1499556725ce29f8c0d35c11052c274fadb3
Lucas <lucasbertolo2@gmail.com>
2021-08-31 11:44:11 -0300

    adicionando configuraçoes iniciais websocket

--- src/api/corrida/corrida.module.ts
+++ src/api/corrida/corrida.module.ts
@@ -1,35 +1,32 @@
-import { CorridaService } from './corrida.service';
+import { CorridaService } from "./corrida.service";
+import { CorridaGateway } from "./corrida.gateway";
+
 /*
 https://docs.nestjs.com/modules
 */
 
-import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { CorridaController } from './corrida.controller';
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { CorridaController } from "./corrida.controller";
 
 @Module({
-    imports: [
-
-        ClientsModule.register([
-            {
-              name: 'KAFKA_SERVICE',
-              transport: Transport.KAFKA,
-              options: {
-                client: {
-                  brokers: ['broker:29092'],
-                },
-                consumer: {
-                  groupId: 'my-consumer-' + Math.random(),
-                },
-              },
-            },
-          ]),
-
-    ],
-    controllers: [
-        CorridaController,
-    ],
-    providers: [
-        CorridaService,],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "KAFKA_SERVICE",
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ["broker:29092"],
+          },
+          consumer: {
+            groupId: "my-consumer-" + Math.random(),
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [CorridaController],
+  providers: [CorridaService, CorridaGateway],
 })
-export class CorridaModule { }
+export class CorridaModule {}
